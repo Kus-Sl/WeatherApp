bab22a8f72b99b4a068e8cb1eba2ed3310ce18ec
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-06-18 22:35:25 +0300

    Creating base layout of main screen

--- WeatherApp/ViewControllers/MainViewController.swift
+++ WeatherApp/ViewControllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  WeatherApp
 //
 //  Created by Вячеслав Кусакин on 18.06.2022.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
